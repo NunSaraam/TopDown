d4299df432a848dc7a2fbd1953a1efbd2dd3fc5e
NunSaraam <yjc22950037@gmail.com>
2025-05-25 23:51:52 +0900

    [Feat] 플레이어 움직임, 마우스 월드 좌표 구해서 총이 마우스 바라보게하기

---
+++ Assets/02_Scripts/Aiming.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Aiming : MonoBehaviour
+{
+    [SerializeField] private Transform mouseTransform;
+
+    public Camera mainCamera;
+    public Animator animator;
+
+
+
+    private void Awake()
+    {
+        mainCamera = Camera.main;
+        animator = GetComponent<Animator>();
+        mouseTransform = transform.Find("Aim");
+    }
+
+    private void Update()
+    {
+        HandleAiming();
+        Shooting();
+
+    }
+
+    private void HandleAiming()
+    {
+        Vector3 mousePosition = GetMouseWorldPosition();
+
+        Vector3 mouseDirection = (mousePosition - mouseTransform.position).normalized;
+        float angle = Mathf.Atan2(mouseDirection.y, mouseDirection.x) * Mathf.Rad2Deg;
+        mouseTransform.eulerAngles = new Vector3(0, 0, angle);
+    }
+
+    private void Shooting()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            animator.SetTrigger("Shoot");
+            Debug.Log("트리거 발동");
+        }
+    }
+
+    public Vector3 GetMouseWorldPosition()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        mousePos.z = -mainCamera.transform.position.z;
+        return mainCamera.ScreenToWorldPoint(mousePos);
+    }
+}
---
+++ Assets/02_Scripts/Player.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Animator animator;
+    Rigidbody2D rb;
+
+    public float moveSpeed = 10f;
+    public int maxHealth = 10;
+    public int currentHealth;
+
+    bool isDead = false;
+
+    private float moveHorizontal, moveVertical;
+    Vector2 movement;
+    private int facingDirection = 1;    //1 = ¿À¸¥ÂÊ, -1 = ¿ÞÂÊ
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+            Hit(10);
+
+
+        moveHorizontal = Input.GetAxisRaw("Horizontal");
+        moveVertical = Input.GetAxisRaw("Vertical");
+
+        movement = new Vector2 (moveHorizontal, moveVertical).normalized;
+
+        animator.SetFloat("Velocity", movement.magnitude);
+
+        if (movement.x != 0)
+            facingDirection = movement.x > 0 ? 1 : -1;
+
+        transform.localScale = new Vector2(facingDirection, 1);
+                   
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = movement * moveSpeed;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+    }
+
+    void Hit(int damage)
+    {
+        animator.SetTrigger("Hit");
+        currentHealth -= damage;
+        
+    }
+
+    void Dead()
+    {
+        isDead = true;
+        //call GameOver
+    }
+}
