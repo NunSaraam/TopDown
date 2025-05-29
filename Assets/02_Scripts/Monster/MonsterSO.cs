13c0c03a649f81088262f25ab8249a7fb7abe407
NunSaraam <yjc22950037@gmail.com>
2025-05-30 00:01:55 +0900

    [Feat] SO 추가 및 보스 드랍 데이터 작성

---
+++ Assets/02_Scripts/Monster/MonsterSO.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(menuName = "Monster/MonsterData", fileName = "NewMonster")]
+public class MonsterSO : ScriptableObject
+{
+    public string monsterName;
+    public Sprite monsterIcon;
+    public GameObject monsterPrefab;
+    
+    public int monsterHealth;
+    public float moveSpeed;
+    public int damage;
+    public float attackRange;
+    public float attackCooldown;
+
+    public RuntimeAnimatorController animatorController;
+    public GameObject HealthItemPrefab;
+
+    public bool canSkill;
+    public float skillCooldown;
+}
