d54a0d64f9c00c66969542365b0b9d97c3f0662e
NunSaraam <yjc22950037@gmail.com>
2025-06-24 00:29:31 +0900

    [feat] 문제 수정 완료 자고 일어나서 보스스크립트만 만들어라 자식아

--- Assets/02_Scripts/Manager/UIManager.cs
+++ Assets/02_Scripts/Manager/UIManager.cs
@@ -16,14 +16,6 @@ public class UIManager : MonoBehaviour
     public Sprite fullHealth;
     public Image[] healths;
 
-    [Header("무기 선택UI")]
-    [SerializeField] GameObject weaponSelectPanel;
-    [SerializeField] Transform weaponButtonParent;
-    [SerializeField] GameObject weaponButtonPrefab;
-
-    private WeaponSO selectedWeapon;
-    private GameObject selectedButton = null;
-
 
     private void Awake()
     {
@@ -33,13 +25,8 @@ public class UIManager : MonoBehaviour
             return; 
         }
         Instance = this;
-        DontDestroyOnLoad(gameObject);
     }
 
-    private void Start()
-    {
-        
-    }
 
     public void UpdataHealthUI(int currentHealth, int maxHealth)
     {
@@ -58,67 +45,4 @@ public class UIManager : MonoBehaviour
         }
     }
 
-    public void ShowWeaponSelectUI()
-    {
-        weaponSelectPanel.SetActive(true);
-        GenerateWeaponButtons();
-    }
-
-    void GenerateWeaponButtons()
-    {
-        foreach (Transform child in weaponButtonParent)
-        {
-            Destroy(child.gameObject);
-        }
-
-        var weapons = WeaponInventoryManager.Instace.unlockedWeapons;
-
-        foreach (WeaponSO weapon in weapons)
-        {
-            GameObject btn = Instantiate(weaponButtonPrefab, weaponButtonParent);
-            
-            Image iconImage = btn.transform.Find("IconImage").GetComponent<Image>();
-            TMP_Text nameText = btn.transform.Find("NameText").GetComponent<TMP_Text>();
-            
-            iconImage.sprite = weapon.weaponIcon;
-            nameText.text = weapon.weaponName;
-
-            btn.GetComponent<Button>().onClick.AddListener(() =>
-            {
-                SelectWeapon(weapon, btn);
-            });
-
-            var outline = btn.transform.Find("Panel").GetComponent<Outline>();
-            if (outline != null) outline.enabled = false;
-        }
-    }
-
-    void SelectWeapon(WeaponSO weapon, GameObject btn)
-    {
-
-        selectedWeapon = weapon;
-        if (selectedButton != null)
-        {
-            var prevOutline = selectedButton.transform.Find("Panel").GetComponent<Outline>();
-            if (prevOutline != null) prevOutline.enabled = false;
-        }
-
-        var currOnutline = btn.transform.Find("Panel").GetComponent<Outline>();
-        if (currOnutline != null) currOnutline.enabled = true;
-
-        selectedButton = btn;
-        Debug.Log($"선택된 무기: {weapon.weaponName}");
-    }
-
-    public void OnStartGame()
-    {
-        if (selectedWeapon == null)
-        {
-            Debug.LogWarning("무기를 선택해주세요!");
-            return;
-        }
-
-        GameSessionData.Instance.SetSelectedWeapon(selectedWeapon);
-        SceneManager.LoadScene("Stage");
-    }
 }
---
+++ Assets/02_Scripts/Manager/WeaponSelectUI.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class WeaponSelectUI : MonoBehaviour
+{
+    [Header("무기 선택UI")]
+    [SerializeField] GameObject weaponSelectPanel;
+    [SerializeField] Transform weaponButtonParent;
+    [SerializeField] GameObject weaponButtonPrefab;
+
+    private WeaponSO selectedWeapon;
+    private GameObject selectedButton;
+
+    private void Start()
+    {
+        GenerateWeaponButtons();    
+    }
+
+    public void ShowWeaponSelectUI()
+    {
+        weaponSelectPanel.SetActive(true);
+        GenerateWeaponButtons();
+    }
+
+    void GenerateWeaponButtons()
+    {
+        foreach (Transform child in weaponButtonParent)
+        {
+            Destroy(child.gameObject);
+        }
+
+        var weapons = WeaponInventoryManager.Instace.unlockedWeapons;
+
+        foreach (WeaponSO weapon in weapons)
+        {
+            GameObject btn = Instantiate(weaponButtonPrefab, weaponButtonParent);
+
+            Image iconImage = btn.transform.Find("IconImage").GetComponent<Image>();
+            TMP_Text nameText = btn.transform.Find("NameText").GetComponent<TMP_Text>();
+
+            iconImage.sprite = weapon.weaponIcon;
+            nameText.text = weapon.weaponName;
+
+            btn.GetComponent<Button>().onClick.AddListener(() =>
+            {
+                SelectWeapon(weapon, btn);
+            });
+
+            var outline = btn.transform.Find("Panel").GetComponent<Outline>();
+            if (outline != null) outline.enabled = false;
+        }
+    }
+
+    void SelectWeapon(WeaponSO weapon, GameObject btn)
+    {
+
+        selectedWeapon = weapon;
+        if (selectedButton != null)
+        {
+            var prevOutline = selectedButton.transform.Find("Panel").GetComponent<Outline>();
+            if (prevOutline != null) prevOutline.enabled = false;
+        }
+
+        var currOnutline = btn.transform.Find("Panel").GetComponent<Outline>();
+        if (currOnutline != null) currOnutline.enabled = true;
+
+        selectedButton = btn;
+        Debug.Log($"선택된 무기: {weapon.weaponName}");
+    }
+
+    public void OnStartGame()
+    {
+        if (selectedWeapon == null)
+        {
+            Debug.LogWarning("무기를 선택해주세요!");
+            return;
+        }
+
+        GameSessionData.Instance.SetSelectedWeapon(selectedWeapon);
+        SceneManager.LoadScene("Stage");
+    }
+}
